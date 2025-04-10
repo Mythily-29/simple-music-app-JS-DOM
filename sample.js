6d4d82185324f15ca695c5ede704efd3138df326
Mythily-29 <mythilyadckap.com>
2025-04-10 13:56:44 +0530

    changed

--- sample.js
+++ sample.js
@@ -1,11 +1,12 @@
 let audio = document.getElementById('audio');
 let allbtn=document.querySelectorAll('button');
-let temp=0
+let temp=0;
+let seconds;
 let musicObj = ["Why-this-kolaveri-di.mp3", "Vaathi-Coming-MassTamilan.io.mp3","Rowdy-Baby-MassTamilan.org.mp3",'Enjoy-Enjaami-MassTamilan.io.mp3','Arabic-Kuthu---Halamithi-Habibo-MassTamilan.so.mp3']
 
 function togglefunction(){
   if(audio.paused){
-    timing(audio.duration);$('#play-icon').attr('class','fa-solid fa-pause')
+   $('#play-icon').attr('class','fa-solid fa-pause')
     audio.play()
   }
   else{
@@ -26,7 +27,6 @@ allbtn.forEach(btn=>{
     }
     else if (btn.classList.contains('shuffle')) {
       musicObj = musicObj.sort(() => 0.5 - Math.random())
-      console.log(musicObj)
     }
   })
 })
@@ -42,18 +42,16 @@ function volumehandle(){
 
 let interval;
 
-function timing(duration){
-
-  clearInterval(interval);$('#progress-bar').attr('max',Math.floor(duration))
-  $('#total-time').text(`${Math.floor(duration/60)}:${Math.floor(duration%60)}`)
-  interval=setInterval(()=>{
-  if (audio.paused) return; if(audio.ended) return;
+setInterval(()=>{
+  $('#total-time').text(`${Math.floor(audio.duration/60)}:${Math.floor(audio.duration%60)}`)
+  $('#progress-bar').attr('max',Math.floor(audio.duration))
   $('#progress-bar').val(Math.floor(audio.currentTime));
- 
-  let minutes=Math.floor(audio.currentTime/60);
-  let seconds=Math.floor(audio.currentTime%60);
-  document.getElementById('current-time').textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  seconds=Math.floor(audio.currentTime);
+  document.getElementById('current-time').textContent = `${Math.floor(seconds/60).toString().padStart(2, '0')}:${Math.floor(seconds%60).toString().padStart(2, '0')}`;
 },1000)
+
+function musicrange(){
+  audio.currentTime=$('#progress-bar').val()
 }
 
 
